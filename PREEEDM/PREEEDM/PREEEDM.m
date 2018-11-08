43874251c3195a4a821e2d4f04e15f448b3cce86
Shenglong Zhou <36698633+ShenglongZhou@users.noreply.github.com>
2018-11-08 22:04:12 +0000

    Update PREEEDM.m

--- PREEEDM/PREEEDM/PREEEDM.m
+++ PREEEDM/PREEEDM/PREEEDM.m
@@ -128,8 +128,9 @@ if isfield(pars,'UPPBD')
     U(U==inf)=UB;
 end 
 if isfield(pars,'range')   
-    L(D==0)=(pars.range/scale)^2;                           
-    U(D~=0)=(pars.range/scale)^2;
+    H1 = 1-H; rs =(pars.range/scale)^2
+    L  = L.*H  + H1*rs;                           
+    U  = U.*H1 + H*rs;
 end
 
 L(T,T)       = Z(T,T);      
